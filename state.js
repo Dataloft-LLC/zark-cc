4173fed30b8c1f34218ac120309f2b6211ecb7ff
nathansenn <nathan.senn90@gmail.com>
2022-06-11 06:36:09 -0500

    update starting_block

--- state.js
+++ state.js
@@ -37,7 +37,7 @@ module.exports = {
                 "domain": config.mainAPI,
                 "escrow": true,
                 "escrows": 0,
-                "lastGood": 65086810, //genesisblock
+                "lastGood": 65170512, //genesisblock
                 "marketingRate": 0,
                 "self": [config.leader],
                 "wins": 0,
